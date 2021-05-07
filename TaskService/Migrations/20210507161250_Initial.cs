9277cf75fbdfda42301bb807931f34af54d739c0
Глеб Безруков <bezrukov96@outlook.com>
2021-05-07 19:13:04 +0300

    Добавление миграции

---
+++ TaskService/Migrations/20210507161250_Initial.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskService.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TextTasks",
+                columns: table => new
+                {
+                    Oid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Duration = table.Column<TimeSpan>(type: "time", nullable: false),
+                    SearchWords = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LastModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TextTasks", x => x.Oid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TextTaskResults",
+                columns: table => new
+                {
+                    Oid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FoundedWords = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TextId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TextTaskOid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LastModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TextTaskResults", x => x.Oid);
+                    table.ForeignKey(
+                        name: "FK_TextTaskResults_TextTasks_TextTaskOid",
+                        column: x => x.TextTaskOid,
+                        principalTable: "TextTasks",
+                        principalColumn: "Oid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TextTaskResults_TextTaskOid",
+                table: "TextTaskResults",
+                column: "TextTaskOid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TextTaskResults");
+
+            migrationBuilder.DropTable(
+                name: "TextTasks");
+        }
+    }
+}
